86cab304b6eef486d7c9aec577fde1bbec8d91ee
souravadhikar1 <souravadhikari09296@gmail.com>
2023-04-17 11:57:00 +0600

    intial setup done and heder with navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1lWCX--2PrXbO7513UY-GvFPIX2ibRTI",
+  authDomain: "auth-firebase-context-ta-b0d07.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-b0d07",
+  storageBucket: "auth-firebase-context-ta-b0d07.appspot.com",
+  messagingSenderId: "707258081649",
+  appId: "1:707258081649:web:b662a46cd5fa500b36d6d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
